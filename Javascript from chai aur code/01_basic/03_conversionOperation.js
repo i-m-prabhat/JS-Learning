18b43380fac32a875721947108b50e6f68e05459
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2023-03-09 14:46:20 +0000

    conversion of dataTypes

---
+++ Javascript from chai aur code/01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+// let score = null
+// let score = undefined
+// let score = false
+// let score = "string"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// console.log(typeof score)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+let s = 33
+let ss = String(s);
+console.log(ss);
