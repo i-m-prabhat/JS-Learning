9bcdac3952b943d9acf9f6bd2915b752df0126fc
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2023-03-09 14:14:55 +0000

    datatypes in js

---
+++ Javascript from chai aur code/01_basic/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Prabhat Patel")
+
+let name = "Prabhat"
+let age = 10
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "prabhat")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
