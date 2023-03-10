7dbf1b77bfab3c9e1f701a129e391cbf457330b8
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2023-03-10 02:52:48 +0000

    operations  via operators

--- Javascript from chai aur code/01_basic/03_conversionOperation.js
+++ Javascript from chai aur code/01_basic/03_conversionOperation.js
@@ -15,13 +15,58 @@ let valueInNumber = Number(score)
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
+// "" => false
+// "prabhat" => true
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 let s = 33
 let ss = String(s);
-console.log(ss);
+// console.log(ss);
+
+// *******************************   Operations *****************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " prabhat"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1"+ 2);
+// console.log("1"+ "2");
+// console.log("1"+ 2 + 2);
+// console.log(1+ 2 + "2");
+
+// console.log((5+6)*5%3);
+
+// console.log(true);
+// console.log(+true);
+// console.log("");
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+console.log(num1,num2,num3)
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
