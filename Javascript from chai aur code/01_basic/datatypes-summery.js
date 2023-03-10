d1e4775b7bdfa54cd581580c448076db4a819988
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2023-03-10 03:31:15 +0000

    datatypes in js

---
+++ Javascript from chai aur code/01_basic/datatypes-summery.js
@@ -0,0 +1,48 @@
+/* data types in js
+On the basis of How data stored in memory and how data will be access from memry
+Data types in JavaScript are two types.
+
+1. Primitive DataTypes
+
+    7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+
+2. Non Primitive/Reference Datatypes
+
+    Array, Objects, Functions
+*/
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoogedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 34564581541244545n
+
+// -----------------------------------------------------------------
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj ={
+    name: "prabhat",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber); //bigInt
+console.log(typeof outsideTemp); // null > object
+console.log(typeof scoreValue); // number
+
+console.log(typeof heros)
+console.log(typeof myObj)
