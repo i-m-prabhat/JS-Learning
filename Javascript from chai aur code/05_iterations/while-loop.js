8a730a5e2ce45913fe96b09802c4674d3f3632c7
Prabhat Patel <prabhatpatel508@outlook.com>
2023-06-04 17:48:41 +0530

    while loop

---
+++ Javascript from chai aur code/05_iterations/while-loop.js
@@ -0,0 +1,24 @@
+// while and do while
+
+let index = 0;
+while (index <= 10)
+{
+    // console.log(`Value of index is ${index}`);
+    index = index + 2;
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+let arr = 0;
+while (arr < myArray.length)
+{
+    // console.log(`Value is ${myArray[arr]}`)
+    arr++;
+}
+
+let score = 11;
+do
+{
+    console.log(`Score is ${score}`)
+    score++;
+} while (score < 10);
