a2611db77f3fd4200eb8d40b39f4022796b7a96c
Prabhat Patel <prabhatpatel508@outlook.com>
2023-06-04 11:55:29 +0530

    IIFE functions

---
+++ Javascript from chai aur code/03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions (IIFE);
+
+(() =>
+{
+    console.log("Hello prabhat")
+})();
+
+(function chai()
+{
+    //named IIFE
+    console.log('DB CONNECTED');
+})()
+
+// chai()
