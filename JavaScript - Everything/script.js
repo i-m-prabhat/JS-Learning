f0efade465de62d9310db3937635392b782edcd1
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2025-07-13 20:49:12 +0530

    js Hoisting and other topics

---
+++ JavaScript - Everything/script.js
@@ -0,0 +1,24 @@
+// variables
+
+// var let const
+// declaration vs initialization
+// scope 
+
+// Temporal dead zone // it is the  period of which js know that is exit but can not give the result it give reference error can't access 
+
+console.log(a);
+
+let a = 1;
+
+
+// var -> hoist > set undefined return undefined
+// let -> hoist > set nothing return error
+// const -> hoist > set nothing return error
+
+
+// data types
+// 1. primitive > string, number, boolean, null, undefined, bigint, symbol >> this will not change in the parant
+// 1. reference > array, object, function >> this will chnage the parants after the copy
+
+
+// for checking data type we use typeof for premitive data types and for reference we use instenceof because all references typeof object 
